eb0fe1526255d7394b74189c7008826edd46692b
ishan26gupta <125808746+ishan26gupta@users.noreply.github.com>
2025-07-10 04:20:16 +0530

    Update: added new methods

--- linkedlist.py
+++ linkedlist.py
@@ -74,12 +74,66 @@ class LinkedList:
             temp = temp.next   
         return temp
 
-    # def set_value(self, index, value): 
+    def set_value(self, index, value): 
+        temp = self.get(index)
+        if temp is not None:
+            temp.value = value
+            return True
+        return False
+    
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index-1)
+        new_node.next  = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
+    
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        prev = self.get(index - 1)
+        temp = prev.next
+        prev.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
+    
+    def reverse(self):
+        temp = self.head
+        self.head = self.tail
+        self.tail = temp
+
+        after = temp.next
+        before = None
+
+        for _ in range(self.length):
+            after = temp.next
+            temp.next = before
+            before = temp
+            temp = after
+
+        
 
 
-my_linked_list = LinkedList(2)
+my_linked_list = LinkedList(1)
+my_linked_list.append(2)
+
 my_linked_list.append(3)
+my_linked_list.append(4)
 
-my_linked_list.prepend(1)
+# print(my_linked_list.remove(2))
+
+my_linked_list.reverse()
 
 my_linked_list.print_list()     
